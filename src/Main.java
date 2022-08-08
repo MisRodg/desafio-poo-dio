33ce611dc838a6e345a411c517308912fe818203
MisRodg <mislene.mrr@gmail.com>
2022-08-08 15:07:14 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
+import java.sql.SQLOutput;
 import java.time.LocalDate;
 
 public class Main {
@@ -20,9 +20,44 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devMislene = new Dev();
+        devMislene.setNome("Mislene");
+        devMislene.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Mislene" + devMislene.getConteudosInscritos());
+        devMislene.progredir();
+        devMislene.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Mislene" + devMislene.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Mislene" + devMislene.getConteudosConcluidos());
+        System.out.println("XP:" + devMislene.calcularTotalXp());
+
+        System.out.println("------");
+
+        Dev devLazaro = new Dev();
+        devLazaro.setNome("Lázaro");
+        devLazaro.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Lázaro" + devLazaro.getConteudosInscritos());
+        devLazaro.progredir();
+        devLazaro.progredir();
+        devLazaro.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Lázaro" + devLazaro.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Lázaro" + devLazaro.getConteudosConcluidos());
+        System.out.println("XP:" + devLazaro.calcularTotalXp());
+
+
+
 
     }
 }
